d03e1136f8e5fb42230d22ace75de81e4c976890
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-02-13 21:44:45 +0900

    slackhandler を追加

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/kromiii/tbls-ask-server/tbls"
-)
-
-var (
-	query  string
-	answer string
+	"github.com/kromiii/tbls-ask-server/slackhandler"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
 func main() {
-	query = "ユーザーあたりのポスト数を取得"
-	answer = tbls.Ask(query)
-	fmt.Println(answer)
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+	oauthToken := os.Getenv("SLACK_OAUTH_TOKEN")
+
+	api := slack.New(oauthToken, slack.OptionAppLevelToken(appToken))
+	client := socketmode.New(api)
+
+	slackHandler := slackhandler.SlackHandler{
+		Api: api,
+	}
+
+	go func() {
+		for socketEvent := range client.Events {
+			switch socketEvent.Type {
+			case socketmode.EventTypeConnecting:
+				fmt.Println("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				fmt.Println("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				fmt.Println("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				event, ok := socketEvent.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					continue
+				}
+				client.Ack(*socketEvent.Request)
+				err := slackHandler.HandleCallBackEvent(event)
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}()
+
+	err := client.Run()
+	if err != nil {
+		log.Print(err)
+	}
 }
--- slackhandler/slackhandler.go
+++ slackhandler/slackhandler.go
@@ -2,7 +2,9 @@ package slackhandler
 
 import (
 	"errors"
+	"regexp"
 
+	"github.com/kromiii/tbls-ask-server/tbls"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 )
@@ -15,33 +17,15 @@ func (h *SlackHandler) HandleCallBackEvent(event slackevents.EventsAPIEvent) err
 	innerEvent := event.InnerEvent
 	switch ev := innerEvent.Data.(type) {
 	case *slackevents.AppMentionEvent:
+		q := regexp.MustCompile(`<@U[0-9A-Za-z]+>`).ReplaceAllString(ev.Text, "")
+		a := tbls.Ask(q)
+
 		_, _, err := h.Api.PostMessage(ev.Channel, slack.MsgOptionBlocks(
 			slack.SectionBlock{
 				Type: slack.MBTSection,
 				Text: &slack.TextBlockObject{
-					Type: slack.PlainTextType,
-					Text: "どの都市の天気を調べますか？",
-				},
-				Accessory: &slack.Accessory{
-					SelectElement: &slack.SelectBlockElement{
-						ActionID: "select_city",
-						Type:     slack.OptTypeStatic,
-						Placeholder: &slack.TextBlockObject{
-							Type: slack.PlainTextType,
-							Text: "都市を選択",
-						},
-						Options: []*slack.OptionBlockObject{
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "東京"}, Value: "東京"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "ソウル"}, Value: "ソウル"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "北京"}, Value: "北京"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "シドニー"}, Value: "シドニー"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "パリ"}, Value: "パリ"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "ロンドン"}, Value: "ロンドン"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "ベルリン"}, Value: "ベルリン"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "ニューヨーク"}, Value: "ニューヨーク"},
-							{Text: &slack.TextBlockObject{Type: slack.PlainTextType, Text: "ロサンゼルス"}, Value: "ロサンゼルス"},
-						},
-					},
+					Type: slack.MarkdownType,
+					Text: a,
 				},
 			},
 		))
@@ -50,47 +34,6 @@ func (h *SlackHandler) HandleCallBackEvent(event slackevents.EventsAPIEvent) err
 		}
 	default:
 		return errors.New("unknown event")
-	}
-	return nil
-}
-
-func (h *SlackHandler) HandleInteractionCallback(interaction slack.InteractionCallback) error {
-	if len(interaction.ActionCallback.BlockActions) != 1 {
-		return errors.New("invalid request")
-	}
-
-	action := interaction.ActionCallback.BlockActions[0]
-	switch action.ActionID {
-	case "select_city":
-		weather, err := weather.GetWeather(action.SelectedOption.Value)
-		if err != nil {
-			return err
-		}
-		_, _, _, err = h.Api.SendMessage(
-			"",
-			slack.MsgOptionReplaceOriginal(interaction.ResponseURL),
-			slack.MsgOptionBlocks(
-				slack.SectionBlock{
-					Type: slack.MBTSection,
-					Text: &slack.TextBlockObject{
-						Type: slack.MarkdownType,
-						Text: "どの都市の天気を調べますか？: " + action.SelectedOption.Text.Text,
-					},
-				},
-				slack.SectionBlock{
-					Type: slack.MBTSection,
-					Text: &slack.TextBlockObject{
-						Type: slack.MarkdownType,
-						Text: "```\n" + weather + "```",
-					},
-				},
-			),
-		)
-		if err != nil {
-			return err
-		}
-	default:
-		return errors.New("unknown action")
 	}
 	return nil
 }
