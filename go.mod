0683be2edeff5ba13855d5bbde48921111294c67
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-02-14 22:26:53 +0900

    config を読み込む処理を追加

--- go.mod
+++ go.mod
@@ -114,4 +114,5 @@ require (
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- slackhandler/slackhandler.go
+++ slackhandler/slackhandler.go
@@ -2,35 +2,79 @@ package slackhandler
 
 import (
 	"errors"
-	"regexp"
+	"os"
 
-	"github.com/kromiii/tbls-ask-server/tbls"
+	// "regexp"
+
+	// "github.com/kromiii/tbls-ask-server/tbls"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+	"gopkg.in/yaml.v2"
 )
 
 type SlackHandler struct {
 	Api *slack.Client
 }
 
+type Config struct {
+	Schemas []struct {
+		Name string `yaml:"name"`
+		Path string `yaml:"path"`
+	} `yaml:"schemas"`
+}
+
 func (h *SlackHandler) HandleCallBackEvent(event slackevents.EventsAPIEvent) error {
 	innerEvent := event.InnerEvent
 	switch ev := innerEvent.Data.(type) {
 	case *slackevents.AppMentionEvent:
-		h.Api.PostMessage(
-			ev.Channel,
-			slack.MsgOptionText("ちょっと待ってね（考え中）", false),
-			slack.MsgOptionTS(ev.TimeStamp),
-		)
+		data, err := os.ReadFile("config.yml")
+		if err != nil {
+			return err
+		}
 
-		q := regexp.MustCompile(`<@U[0-9A-Za-z]+>`).ReplaceAllString(ev.Text, "")
-		a := tbls.Ask(q)
+		var config Config
+		err = yaml.Unmarshal(data, &config)
+		if err != nil {
+			return err
+		}
 
-		_, _, err := h.Api.PostMessage(
-			ev.Channel,
-			slack.MsgOptionText(a, false),
-			slack.MsgOptionTS(ev.TimeStamp),
-		)
+		var options []*slack.OptionBlockObject
+		for _, schema := range config.Schemas {
+			options = append(options, &slack.OptionBlockObject{
+				Text:  &slack.TextBlockObject{Type: slack.PlainTextType, Text: schema.Name},
+				Value: schema.Path,
+			})
+		}
+
+		_, _, err = h.Api.PostMessage(ev.Channel, slack.MsgOptionBlocks(
+			slack.SectionBlock{
+				Type: slack.MBTSection,
+				Text: &slack.TextBlockObject{
+					Type: slack.PlainTextType,
+					Text: "対象のスキーマを選んでください",
+				},
+				Accessory: &slack.Accessory{
+					SelectElement: &slack.SelectBlockElement{
+						ActionID: "select_schema",
+						Type:     slack.OptTypeStatic,
+						Placeholder: &slack.TextBlockObject{
+							Type: slack.PlainTextType,
+							Text: "スキーマを選択",
+						},
+						Options: options,
+					},
+				},
+			},
+		))
+
+		// q := regexp.MustCompile(`<@U[0-9A-Za-z]+>`).ReplaceAllString(ev.Text, "")
+		// a := tbls.Ask(q)
+
+		// _, _, err := h.Api.PostMessage(
+		// 	ev.Channel,
+		// 	slack.MsgOptionText(a, false),
+		// 	slack.MsgOptionTS(ev.TimeStamp),
+		// )
 		if err != nil {
 			return err
 		}
