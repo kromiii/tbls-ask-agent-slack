1968b30e544372cef51b2616a33d45deffa61384
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-04-29 12:33:19 +0900

    socket mode を使う

--- main.go
+++ main.go
@@ -1,37 +1,61 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
 	"os"
 
-	"github.com/kromiii/tbls-ask-agent-slack/handler"
 	"github.com/kromiii/tbls-ask-agent-slack/slackhandler"
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
 func main() {
-	// Slack Appの設定画面から取得する
 	oauthToken := os.Getenv("SLACK_OAUTH_TOKEN")
-	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
 
-	api := slack.New(oauthToken)
+	api := slack.New(oauthToken, slack.OptionAppLevelToken(appToken))
+	client := socketmode.New(api)
 
-	// SlackのEventおよびInteractionのハンドラ（再利用するため別定義）
 	slackHandler := slackhandler.SlackHandler{
 		Api: api,
 	}
 
-	http.Handle("/slack/events", &handler.EventHandler{
-		SlackHandler:  &slackHandler,
-		SigningSecret: signingSecret,
-	})
+	go func() {
+		for socketEvent := range client.Events {
+			switch socketEvent.Type {
+			case socketmode.EventTypeConnecting:
+				fmt.Println("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				fmt.Println("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				fmt.Println("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				event, ok := socketEvent.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					continue
+				}
+				client.Ack(*socketEvent.Request)
+				err := slackHandler.HandleCallBackEvent(event)
+				if err != nil {
+					log.Print(err)
+				}
+			case socketmode.EventTypeInteractive:
+				interaction, ok := socketEvent.Data.(slack.InteractionCallback)
+				if !ok {
+					continue
+				}
+				err := slackHandler.HandleInteractionCallback(interaction)
+				if err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}()
 
-	http.Handle("/slack/interaction", &handler.InteractivityHandler{
-		SlackHandler: &slackHandler,
-	})
-
-	err := http.ListenAndServe(":8080", nil)
+	err := client.Run()
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 }
